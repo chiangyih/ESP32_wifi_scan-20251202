f93adf54cd5f94fafdc6118d314f765a237981ab
John Tseng <tseng@ms.hhvs.tn.edu.tw>
2025-12-02 14:39:58 +0800

    重構 main.cpp，新增 WiFi 掃描功能，移除不必要的函數定義

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,31 @@
 #include <Arduino.h>
-
-// put function declarations here:
-int myFunction(int, int);
+#include <WiFi.h>
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
+  delay(1000);
+  
+  // 設定 WiFi 模式為站點模式
+  WiFi.mode(WIFI_STA); // 設定為 Station 模式
+  WiFi.disconnect(true); // 斷開任何現有的連接並清除配置
+  
+  Serial.println("\nESP32 WiFi SSID 掃描\n");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  // 開始掃描 WiFi 網路
+  int networksFound = WiFi.scanNetworks();
+  
+  if (networksFound == 0) {
+    Serial.println("未找到 WiFi 網路");
+  } else {
+    Serial.printf("找到 %d 個 WiFi 網路:\n", networksFound);
+    for (int i = 0; i < networksFound; i++) {
+      Serial.printf("%d. %s\n", i + 1, WiFi.SSID(i).c_str());
+    }
+  }
+  
+  WiFi.scanDelete();
+  Serial.println();
+  delay(5000); // 5 秒後重新掃描
 }
